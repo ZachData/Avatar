e35682fe096538ad028d0d51d5ff0115aac39c91
Zach Baker <37054925+ZachData@users.noreply.github.com>
2021-11-03 22:38:58 -0500

    New version
    incremental update, adds batch to memory

--- delete_repeats.py
+++ delete_repeats.py
@@ -11,70 +11,84 @@ import os
 from PIL import Image, ImageOps
 import numpy as np 
 
-meta_path = r'C:\Users\M\Desktop\delete-repeats\data'
-# im_path = r'/home/zach/Desktop/Snik/Avatar-main/00000'
+meta_path = r'/home/zach/Desktop/Datasets/Avatar_Frames_Clean'
+# meta_path = r'/home/zach/Desktop/Snik/Avatar-main/test_images'
 meta_dir = os.listdir(meta_path)
-# sorted_dir = sorted(im_dir)
-# dir_len = len(im_dir)
-# os.chdir(im_path)
+sorted_meta = sorted(meta_dir)
 
+def get_folders():
+	folder_names = []
+	for folder_name in sorted_meta:
+		if 693 > int(folder_name) or int(folder_name) > 1000:
+			continue
+		folder_names.append(folder_name)
+	return folder_names
+
+def goto_folder(name):
+	im_path = os.path.join(meta_path, name)
+	im_dir = os.listdir(im_path)
+	sorted_folders = sorted(im_dir)
+	return sorted_folders, im_path
 
 def get_image(filename):
 	# get one image
 	image = Image.open(filename)
-	# image = ImageOps.grayscale(image)
 	image = image.resize((16, 16))
 	data = np.asarray(image).astype(float)
 	data = data - data.mean()
 	data = data / (np.linalg.norm(data) + 1e-3)
-	# print(data.mean(), np.linalg.norm(data))
-
 	return data
 
-def mark_repeats(im1, im2, tabs, filename):
-	# compare two images
-	print(np.array_equal(im1, im2))
-		# print(im1[:,:,0].shape, im2[:,:,0].shape)
-		# print('found True')
-		# tabs.append(filename)
+def get_imgset(sorted_folders):
+	#collect all images into ram.
+	img_set = []
+	name_set = []
+	for filename in sorted_folders:
+		im = get_image(filename)
+		img_set.append(im)
+		name_set.append(filename)
+	return img_set, name_set
+
+def subtracter(names, img_set):
+	# if image is too similar (difference near 0), remove the almost repeated image
+	names_to_remove = []
+	# pick an image 
+	for idx in range(len(img_set)):
+		im = img_set[idx]
+		if names[idx] in names_to_remove:
+			continue
+		for comparison_img_idx in range(len(img_set)):
+			comparison_img = img_set[comparison_img_idx]
+			comparison_filename = names[comparison_img_idx]
+			if comparison_filename in names_to_remove:
+				continue
+			color_sums = 0
+			for color in range(3):
+				difference = np.subtract(im, comparison_img)
+				difference = np.absolute(difference)
+				sums = np.sum(difference[:,:,color])
+				color_sums += sums
+			if 0.0001 < sums < 0.3:
+				# diff image ~ 10, same ~0.05
+				names_to_remove.append(comparison_filename)
+	return names_to_remove
+
+def remover(names_to_remove):
+	# delete filenames in names_to_remove
+	print(f'removing {len(names_to_remove)} images.')
+	[os.remove(removal_name) for removal_name in names_to_remove]
 
 def main():
-	# collect filenames to delete in tabs
-	# track to only go forwards, removing repeats
-	for folder_name in meta_dir:
-		print(folder_name)
-		im_path = os.path.join(meta_path, folder_name)
-		im_dir = os.listdir(im_path)
-		sorted_dir = sorted(im_dir)
-		dir_len = len(im_dir)
+	# metafolder -> folders -> images -> find similar images -> remove them
+	folder_names = get_folders()
+	for name in folder_names:
+		print(name)
+		sorted_folders, im_path = goto_folder(name)
 		os.chdir(im_path)
-		tabs = []
-		im_tracker = 0
-		for filename in sorted_dir:
-			im_tracker += 1
-			im = get_image(filename)
-			for comparison_filename in sorted_dir[im_tracker:dir_len]:
-				if comparison_filename in tabs:
-					continue
-				comparison_img = get_image(comparison_filename)
-				color_sums = 0
-				for color in range(3):
-					difference = np.subtract(im, comparison_img)
-					difference = np.absolute(difference)
-					sums = np.sum(difference[:,:,color])
-					color_sums += sums
-				if 0.0001 < sums < 0.3:
-					# diff image ~ 10, same ~0.05
-					tabs.append(comparison_filename)
-				# print(filename)
+		img_set, name_set = get_imgset(sorted_folders)
+		names_to_remove = subtracter(name_set, img_set)
+		# print(names_to_remove)
+		remover(names_to_remove)
 
-		# delete filenames in tabs
-		print(len(tabs))
-		for repeat in tabs:
-			os.remove(repeat)
-
-main()
-
-
-# for i in sorted_dir:
-# 	print(i)
+if __name__ == "__main__":
+	main()
